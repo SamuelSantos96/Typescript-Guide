327c6cb766fdc587e7e7977aa46629f9d291a639
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-08 16:40:31 +0000

    Inference.

--- features/annotations/variables.js
+++ features/annotations/variables.js
@@ -1,11 +1,12 @@
 // Variables
-var apple = 5;
+var apple = 5; // Typescript knows the type of the variable because it was declared and initialized in the same statment
 var speed = 'fast';
 var hasName = true;
 var nothingMuch = null;
 var nothing = undefined;
 // Built in objects
 var now = new Date();
+// Arrays
 var colors = ['red', 'green', 'blue'];
 var myNumbers = [1, 2, 3];
 var truths = [true, true, false];
@@ -21,3 +22,29 @@ var point = {
     x: 10,
     y: 20
 };
+// Functions
+var logNumber = function (i) {
+    console.log(i);
+};
+// When to use annotations
+// 1) Function that returns the 'any' type
+var json = '{"x": 10, "y": 20}';
+var coordinates = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+// 2) When we declare a variable on one line
+// and initializate it later
+var words = ['red', 'green', 'blue'];
+var foundWord;
+for (var i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+// 3) Variable whose type cannot be inferred correctly
+var numbers = [-10, -1, 12];
+var numberAboveZero = false;
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,5 +1,5 @@
 // Variables
-let apple: number = 5;
+let apple = 5; // Typescript knows the type of the variable because it was declared and initialized in the same statment
 let speed: String = 'fast';
 let hasName: boolean = true;
 
@@ -28,3 +28,30 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
     console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
+
+// 2) When we declare a variable on one line
+// and initializate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+        foundWord = true;
+    }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
