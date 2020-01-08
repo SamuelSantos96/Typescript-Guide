dc2b3695e79bb83d0e7a8ac5af09dfef979a6bb5
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-08 15:10:13 +0000

    Fetch data from url. Storing data using variable based on an Interface.

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,22 @@ import axios from 'axios';
 
 const url = 'http://jsonplaceholder.typicode.com/todos/1';
 
+interface TODO {
+    id: number;
+    title: String;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    console.log(response.data);
+    const todo = response.data as TODO;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+        `);
 });
