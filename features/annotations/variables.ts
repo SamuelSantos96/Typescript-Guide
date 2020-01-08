1b9a6b1b7765512d9e6d37942673eaa6660d7a17
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-08 15:50:10 +0000

    Type Annotations and Variables. Annotations with Variables.

---
+++ features/annotations/variables.ts
@@ -0,0 +1,9 @@
+let apple: number = 5;
+let speed: String = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// Built in objects
+let now: Date = new Date();
