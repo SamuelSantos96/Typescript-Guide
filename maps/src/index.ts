dd2768f85d2ce65df28a3c567ce303e1d966a4a3
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-16 16:08:44 +0000

    Google Maps Integration.

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,17 @@
-import { User } from './User';
+import 'googlemaps';
+// import { User } from './User';
+// import { Company } from './Company';
 
-const user = new User();
+// const user = new User();
+// const company = new Company();
 
-console.log(user);
+// console.log(user);
+// console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
