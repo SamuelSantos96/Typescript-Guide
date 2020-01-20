9bea4853853464d387ccdb048acd9b4fef02c80b
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-20 10:51:22 +0000

    Adding Markers: Possible solution.

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -84988,22 +84988,12 @@ function () {
     });
   }
 
-  CustomMap.prototype.addUserMarker = function (user) {
+  CustomMap.prototype.addMarker = function (mappable) {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng
-      }
-    });
-  };
-
-  CustomMap.prototype.addCompanyMarker = function (company) {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
       }
     });
   };
@@ -170070,8 +170060,7 @@ var Company_1 = require("./Company"); // console.log(user);
 var user = new User_1.User();
 var company = new Company_1.Company();
 var customMap = new CustomMap_1.CustomMap('map');
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addUserMarker(user); // customMap.addCompanyMarker(company);
 },{"googlemaps":"node_modules/googlemaps/lib/index.js","./CustomMap":"src/CustomMap.ts","./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -170100,7 +170089,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62043" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62292" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -11,4 +11,4 @@ const company = new Company();
 const customMap = new CustomMap('map');
 
 customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+// customMap.addCompanyMarker(company);
