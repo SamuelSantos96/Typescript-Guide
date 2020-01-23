b07b5f4dc89fa59c9b830dc317845d9124cb99c9
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-23 12:08:08 +0000

    Solved problem with gitignore. Since the build directory was already tracked by git in previous commits it was necessary to remove it before adding it to the gitignore.

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,5 +1,23 @@
-console.log(123);
+class Sorter {
+    // Making the collection public is the same as defining the collection as an attribute of Sorter
+    constructor(public collection: number[]) {}
 
-const logSomething = () => {
-    console.log('Something');
-};
+    sort(): void {
+        // Destructuring assignment as ES6
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
