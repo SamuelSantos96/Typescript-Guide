ac8640b82ec76f80c633b216c129668bb823cb5e
Samuel Santos <samuel.trabalho.96@gmail.com>
2020-01-29 11:22:40 +0000

    Type Guards.

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,6 @@
 class Sorter {
     // Making the collection public is the same as defining the collection as an attribute of Sorter
-    constructor(public collection: number[]) {}
+    constructor(public collection: number[] | string) {}
 
     sort(): void {
         // Destructuring assignment as ES6
@@ -8,10 +8,22 @@ class Sorter {
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    const temp = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = temp;
+                // Only the properties that number[] and string have in common are accessible here
+
+                // If collection is an array of numbers (Type Guard)
+                if (this.collection instanceof Array) {
+                    // All the properties of number[] are accessible here
+
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const temp = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = temp;
+                    }
+                }
+
+                // If collection is a string (Type Guard)
+                if (typeof this.collection === 'string') {
+                    // All properties of string are accessible here
                 }
             }
         }
